307ff4867c9fd3fc7ff44afc58096ed827033cd4
anzechen <anzechen@bu.edu>
2026-04-24 22:29:50 -0400

    4.2 Fixed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn safe_divide(a:f64,b:f64) -> Option<f64>{
-    match b:f64{
-        b = 0.0 => Err("Can't divide by zero"),
+    match b{
+        0.0 => None,
         _ => Some(a/b),
+    }
+}
+
+fn main(){
+    match safe_divide(10.0,24.0){
+        Some(result) => println!("Result: {}", result),
+        None => println!("Error: cannot divide by zero"),
     }
 }
