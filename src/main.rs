03059bed97ab720d4c1b7ab539498c0d67f8bd92
anzechen <anzechen@bu.edu>
2026-04-24 23:02:40 -0400

    Wrong 5.2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-fn count_words(s:String){
-    let list = s.split_whitespace();
-    let counts = list.filter(|x|x.len()>3).count();
-    println!("{}", counts);
+struct StringBuilder{
+    c=content:String,
 }
 
-fn main(){
-    let sentence:String = "the quick brown fox jumps over the lazy dog".to_string();
-    count_words(sentence);
+impl StringBuilder{
+    fn new() -> StringBuilder{
+        let c = StringBuilder("")
+        c}
+        ///Sorry, I don't remember why would I wrote a c in here. It doesn't make any sense to me right now.
+    fn add(&mut self, text:&str){
+            let new_string = self.push_str(text);
+            return new_string;
+        }
+    fn build(&self) -> String{
+            let copy = self.clone();
+            return copy;}
+        }
+fn main(){let word=""::StringBuilder::new();
+        let new_word = word::StringBuilder::add("Hello, ");
+        let new_word = new_word::StringBuilder::add("World!");
+        println!("{:?}",new_word);
 }
