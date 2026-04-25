ecf3aa9d4bf5f3f4835865e12d1172c423bf2e30
anzechen <anzechen@bu.edu>
2026-04-24 22:55:11 -0400

    Fixed 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn count_words(s:String){
     let list = s.split_whitespace();
-    let counts = list.iter().(filter|x|x.len()>3).count();
+    let counts = list.filter(|x|x.len()>3).count();
     println!("{}", counts);
+}
+
+fn main(){
+    let sentence:String = "the quick brown fox jumps over the lazy dog".to_string();
+    count_words(sentence);
 }
