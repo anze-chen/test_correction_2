53efb5353b098b0fc94bdf5b2f4fdd467b1b1023
anzechen <anzechen@bu.edu>
2026-04-24 22:32:57 -0400

    Wrong 4.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let v1 = vec![1.0,2.0,3.0];
+    let v2 = vec![4.0,5.0,6.0];
+    let dp = v1.iter().v1.zip(v2).map(|v1,v2|v1*v2).sum();
+    println! ("{}",dp);
 }
