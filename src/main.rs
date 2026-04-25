ab77bdf3eeed3f3f43c856d1e8955b799e255367
anzechen <anzechen@bu.edu>
2026-04-24 22:23:06 -0400

    wrong version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+fn safe_divide(a:f64,b:f64) -> Option<f64>{
+    match b:f64{
+        b = 0.0 => Err("Can't divide by zero"),
+        _ => Some(a/b),
+    }
 }
